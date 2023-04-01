390cc238bdaef6366912d12f835cd88465e1d7b1
moroz69off <moroz69off@gmail.com>
2023-04-02 00:53:35 +0300

    repair ui

--- MRZmLearnAPOD/General.cs
+++ MRZmLearnAPOD/General.cs
@@ -12,9 +12,21 @@ namespace MRZmLearnAPOD
 {
     public partial class General : Form
     {
+        // The objective of the NASA API portal is to make NASA data, including imagery, eminently accessible to application developers.
+        const string EndpointURL = "https://api.nasa.gov/planetary/apod";
+
+        // June 16, 1995: the APOD launch date.
+        DateTime launchDate = new DateTime(1995, 6, 16);
+
+        // A count of images downloaded today.
+        int imageCountToday;
+
         public General()
         {
             InitializeComponent();
+            // Set the maximum date to today and the minimum date to the APOD launch date.
+            MonthCalendar.MaxDate = DateTime.Today;
+            MonthCalendar.MinDate = launchDate;
         }
     }
 }
