5c397b492c109fdbc2c45f5fe255cdad4b59b3fd
moroz69off <moroz69off@gmail.com>
2023-04-03 21:59:49 +0300

    return to apod

--- MRZmLearnAPOD_UWP/MainPage.xaml.cs
+++ MRZmLearnAPOD_UWP/MainPage.xaml.cs
@@ -12,6 +12,11 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml.Media.Imaging;
+using Newtonsoft.Json.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
 
 namespace MRZmLearnAPOD_UWP
 {
@@ -19,25 +24,36 @@ namespace MRZmLearnAPOD_UWP
     {
         DateTime launchDate = new DateTime(2014, 02, 01);
 
-        const string EndpointURL = "https://api.nasa.gov/planetary/earth/imagery";
-        const string GIBS = "Global Imagery Browse Service";
-        const string exampleQuery = "https://api.nasa.gov/planetary/earth/imagery?lon=100.75&lat=1.5&date=2014-02-01&api_key=DEMO_KEY";
+        const string EndpointURL = "https://api.nasa.gov/planetary/apod";
 
         float dim, lat, lon;
 
+        private bool IsSupportedFormat(string photoURL)
+        {
+            // Extract the extension and force to lower case for comparison purposes.
+            string ext = Path.GetExtension(photoURL).ToLower();
+
+            // Check the extension against supported UWP formats.
+            return ext == ".jpg" || ext == ".jpeg" || ext == ".png" || ext == ".gif" ||
+                   ext == ".tif" || ext == ".bmp"  || ext == ".ico" || ext == ".svg";
+        }
+
         private void LatTB_TextChanged(object sender, TextChangedEventArgs e)
         {
-            lat = float.Parse(LatTB.Text);
+            //lat = float.Parse(LatTB.Text);
         }
 
         private void LonTB_TextChanged(object sender, TextChangedEventArgs e)
         {
-            lon = float.Parse(LonTB.Text);
+            //lon = float.Parse(LonTB.Text);
         }
 
-        private void LaunchButton_Click(object sender, RoutedEventArgs e)
+        private async void LaunchButton_Click(object sender, RoutedEventArgs e)
         {
+
             MonthCalendar.MinDate = launchDate;
+
+            await RetrievePhoto();
 
             //LatTB.Text = lat.ToString();
             //LonTB.Text = lon.ToString();
@@ -46,13 +62,78 @@ namespace MRZmLearnAPOD_UWP
         public MainPage()
         {
             this.InitializeComponent();
+
             dim = 0.025F;
             lat = 47.2070208F;
             lon = 39.6867104F;
 
+            //lat = 47.20F;
+            //lon = 39.68F;
+
             MonthCalendar.MinDate = launchDate;
             MonthCalendar.MaxDate = DateTime.Today;
 
+        }
+
+        private async Task RetrievePhoto()
+        {
+            var client = new HttpClient();
+            JObject jResult = null;
+            string responseContent = null;
+            string photoUrl = null;
+
+
+            // Build the date parameter string for the date selected, or the last date if a range is specified.
+            DateTimeOffset dt = (DateTimeOffset)MonthCalendar.Date;
+
+            string dateSelected = $"{dt.Year.ToString()}-{dt.Month.ToString("00")}-{dt.Day.ToString("00")}";
+
+            string URLParams = $"?date={dateSelected}&api_key=DEMO_KEY";
+
+            // Populate the HTTP client appropriately.
+            client.BaseAddress = new Uri(EndpointURL);
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            // The critical call: send a GET request with the appropriate parameters.
+            HttpResponseMessage response = client.GetAsync(URLParams).Result;
+
+            if (response.IsSuccessStatusCode)
+            {
+                // Be ready to catch any data or server errors.
+                try
+                {
+                    // Parse the response by using the Newtonsoft APIs.
+                    responseContent = await response.Content.ReadAsStringAsync();
+
+                    // Parse the response string for the details we need.
+                    jResult = JObject.Parse(responseContent);
+
+                    // Now get the image.
+                    photoUrl = (string)jResult["url"];
+                    var photoURI = new Uri(photoUrl);
+                    var bmi = new BitmapImage(photoURI);
+
+                    ImagePicture.Source = bmi;
+
+                    if (IsSupportedFormat(photoUrl))
+                    {
+
+                    }
+                    else
+                    {
+
+                    }
+                }
+                catch (Exception ex)
+                {
+
+                }
+
+            }
+            else
+            {
+
+            }
         }
     }
 }
