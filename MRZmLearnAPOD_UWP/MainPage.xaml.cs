fa9e8e4c9e404cfc43350811ab0c0b0298e05abe
moroz69off <moroz69off@gmail.com>
2023-04-03 18:57:14 +0300

    lat&lon ok

--- MRZmLearnAPOD_UWP/MainPage.xaml.cs
+++ MRZmLearnAPOD_UWP/MainPage.xaml.cs
@@ -23,19 +23,36 @@ namespace MRZmLearnAPOD_UWP
         const string GIBS = "Global Imagery Browse Service";
         const string exampleQuery = "https://api.nasa.gov/planetary/earth/imagery?lon=100.75&lat=1.5&date=2014-02-01&api_key=DEMO_KEY";
 
-        float dim = 0.025F, lat = 47.2070208F, lon = 39.6867104F;
+        float dim, lat, lon;
+
+        private void LatTB_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void LonTB_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
 
         private void LaunchButton_Click(object sender, RoutedEventArgs e)
         {
             MonthCalendar.MinDate = launchDate;
+
+            LatTB.Text = lat.ToString();
+            LonTB.Text = lon.ToString();
         }
 
         public MainPage()
         {
             this.InitializeComponent();
+            dim = 0.025F;
+            lat = 47.2070208F;
+            lon = 39.6867104F;
 
             MonthCalendar.MinDate = launchDate;
             MonthCalendar.MaxDate = DateTime.Today;
+
         }
     }
 }
